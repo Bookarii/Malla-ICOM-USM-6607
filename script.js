89b1f994cf0b6623b433c504cd65e6c276458e0f
Bookarii <flooppy2005@gmail.com>
2026-07-24 15:04:18 -0400

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-console.log("La página funciona correctamente.");
+// ======================================
+// Crear las columnas de los semestres
+// ======================================
+
+const contenedor = document.getElementById("malla");
+
+for (let semestre = 1; semestre <= 10; semestre++) {
+
+    const columna = document.createElement("div");
+
+    columna.className = "semestre";
+
+    columna.innerHTML = `
+        <div class="titulo-semestre">
+            ${semestre}
+        </div>
+
+        <div class="contenedor-ramos"
+             id="semestre-${semestre}">
+        </div>
+    `;
+
+    contenedor.appendChild(columna);
+
+}
