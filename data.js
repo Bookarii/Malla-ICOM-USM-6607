27d6f66d0b1c629b8d0956404cc2949c41839faf
Bookarii <flooppy2005@gmail.com>
2026-07-24 15:23:57 -0400

    data.js

--- data.js
+++ data.js
@@ -1,1 +1,17 @@
-const ramos = [];
+// =====================================
+// Datos de los ramos
+// =====================================
+
+const ramos = [
+
+    {
+        id: 1,
+        sigla: "ICS111",
+        nombre: "Administración de Empresas",
+        semestre: 1,
+        area: "administracion",
+        creditos: 5,
+        prerrequisitos: []
+    }
+
+];
